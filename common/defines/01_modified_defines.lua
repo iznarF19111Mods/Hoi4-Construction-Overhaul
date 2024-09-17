04a73c5f489f39c657cee0fd4f077a6fd1a410fe
iznarF19111 <iznarf19111@gmail.com>
2024-09-18 00:32:20 +0200

    modified civs
    Modified the DEFINES for increased numbers of civilian factories and tried to implement a building that also provides the same capabilities as a civilian factory. The latter did not work as intended, but the added files were not removed.

---
+++ common/defines/01_modified_defines.lua
@@ -0,0 +1,5 @@
+NDefines.NGame.END_DATE = "2100.1.1.1"  -- Considering the maximum amount of Factories, this seems appropriate.
+NDefines.NProduction.MAX_CIV_FACTORIES_PER_LINE = 75
+NDefines.NProduction.BASE_FACTORY_SPEED = 1
+NDefines.NProduction.MINIMUM_NUMBER_OF_FACTORIES_TAKEN_BY_CONSUMER_GOODS_PERCENT = 0.01
+NDefines.NMarket.MAX_CIV_FACTORIES_PER_CONTRACT = 750
